f27f7357125fd05605662e2c8d1c1643b0ef48a2
Raudeck <70005949+Raudeck@users.noreply.github.com>
2021-05-14 22:18:57 +0800

    Init

---
+++ trace.go
@@ -0,0 +1,13 @@
+// +build go1.8
+
+package websocket
+
+import (
+	"net/http/httptrace"
+
+	tls "github.com/refraction-networking/utls"
+)
+
+func doHandshakeWithTrace(trace *httptrace.ClientTrace, tlsConn *tls.UConn, cfg *tls.Config) error {
+	return doHandshake(tlsConn, cfg)
+}
